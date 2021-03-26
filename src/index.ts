230ab62f7b6726d9ddbb9f04fa07a74ec724e36c
Mathis Barré <mathis.barre@live.fr>
2021-03-26 14:13:49 +0100

    feat: reorganise project

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
-import express from 'express'
+import express, { Application } from 'express'
 import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
+import indexRouter from './routes/index.route'
+import debug from 'debug'
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+(() => {
+  const app = express()
+  setupConfiguration(app)
+  setupMiddleware(app)
+  setupRoutes(app)
+  startApplication(app)
+})()
 
-const port = process.env.port ?? 3000
-app.listen(port, () => {
-  console.log('🚀 The application is launched on https://localhost:3000, it is now ready to go to the moon !')
-})
+function setupConfiguration (app: Application) {
+  dotenv.config()
+}
+
+function setupMiddleware (app: Application) {
+}
+
+function setupRoutes (app: Application) {
+  app.use('/', indexRouter)
+}
+
+function startApplication (app: Application) {
+  const port = process.env.APP_PORT ?? 3000
+  const message = `🚀 The application is launched on http://localhost:${port}`
+  app.listen(port, () => { console.log(message) })
+}
