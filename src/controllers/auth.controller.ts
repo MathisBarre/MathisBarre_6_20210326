300396cd013b57069146138815da8bf4f4278466
Mathis Barré <mathis.barre@live.fr>
2021-04-04 16:14:41 +0200

    refactor: remove callbacks hell

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,16 +2,16 @@ import { Response, Request, NextFunction } from 'express'
 import bcrypt from 'bcrypt'
 import User from '../models/user.model'
 
-export function signup (req: Request, res: Response, next: NextFunction) {
-  const { email, password } = req.body
-  bcrypt.hash(password, 10, (error: Error, hash: string) => {
-    if (error) next(error)
-
-    User.create({ email, password: hash }, (error, user) => {
-      if (error) next(error)
-      res.json(user)
-    })
-  })
+export async function signup (req: Request, res: Response, next: NextFunction) {
+  try {
+    const email: string = req.body.email
+    const password: string = req.body.password
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = await User.create({ email, password: hashedPassword })
+    if (newUser) res.json(newUser)
+  } catch (error) {
+    next(error)
+  }
 }
 
 export function login (req: Request, res: Response) {
