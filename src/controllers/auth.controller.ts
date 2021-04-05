27a4bfad806064c7bd5b465de9f0ab1749c4d618
Mathis Barré <mathis.barre@live.fr>
2021-04-05 15:34:19 +0200

    feat: login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -14,5 +14,36 @@ export async function signup (req: Request, res: Response, next: NextFunction): 
   }
 }
 
-export function login (req: Request, res: Response): void {
+export async function login (req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const emailInRequest: string = req.body.email
+    const passwordInRequest: string = req.body.password
+
+    const userInDB = await User.findOne({ email: emailInRequest })
+
+    if (userInDB === null || userInDB.password === null) {
+      res.status(401).json({
+        message: 'Access denied : no account is linked to this email address',
+        message_FR: 'Accès réfusé : aucun compte n\'est lié à cette adresse e-mail'
+      })
+      return
+    }
+
+    const hashedPasswordInDB = userInDB.password
+    const thePasswordsMatch = await bcrypt.compare(passwordInRequest, hashedPasswordInDB)
+
+    if (thePasswordsMatch) {
+      res.json({
+        message: 'Access allowed',
+        jwtToken: 's57df5qs7dfqsdf57qsdf.6qsd7fqs87df8sq7df8'
+      })
+    } else {
+      res.status(401).json({
+        message: 'Access denied : bad password',
+        message_FR: 'Accès refusé : mauvais mot de passe'
+      })
+    }
+  } catch (error) {
+    next(error)
+  }
 }
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import { signup, login } from '../controllers/auth.controller'
 
 export default Router()
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   .post('/signup', signup)
   .post('/login', login)
