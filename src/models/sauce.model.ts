f1be72993f1b35d6a6f10b885bbe1fbf22161f70
Mathis Barré <mathis.barre@live.fr>
2021-04-10 11:05:37 +0200

    feat: create sauce model

---
+++ src/models/sauce.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model, Document, Model } from 'mongoose'
+import uniqueValidator from 'mongoose-unique-validator'
+
+export interface ISauce extends Document {
+  _id: string
+  userId: string
+  name: string
+  manufacturer: string
+  description: string
+  mainPepper: string
+  imageUrl: string
+  heat: number
+  likes: number
+  dislikes: number
+  usersLiked: string[]
+  usersDisliked: string[]
+}
+
+const sauceSchema = new Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true, default: 0 },
+  dislikes: { type: Number, required: true, default: 0 },
+  usersLiked: [{ type: String, required: true, default: [] }],
+  usersDisliked: [{ type: String, required: true, default: [] }]
+})
+
+sauceSchema.plugin(uniqueValidator)
+
+const Sauce: Model<ISauce> = model('Sauce', sauceSchema)
+
+export default Sauce
