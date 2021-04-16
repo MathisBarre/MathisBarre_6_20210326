15898e0e22bf1e3a00c0326514f1a81dc0a39f01
Mathis Barré <mathis.barre@live.fr>
2021-04-16 12:09:23 +0200

    feat: getOne & deleteOne

--- src/controllers/sauces.controller.ts
+++ src/controllers/sauces.controller.ts
@@ -1,5 +1,6 @@
 import { Response, Request, NextFunction } from 'express'
 import Sauces from '../models/sauce.model'
+import path from 'path'
 
 export async function getAll (req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
@@ -10,11 +11,23 @@ export async function getAll (req: Request, res: Response, next: NextFunction): 
   }
 }
 
+export async function getOne (req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const sauceIdInRequest = req.params.id
+    const sauceToReturn = await Sauces.findOne({ _id: sauceIdInRequest })
+    if (sauceToReturn === null) throw new Error('There is no existing sauce with this id')
+    res.json(sauceToReturn)
+  } catch (error) {
+    next(error)
+  }
+}
+
 export async function createOne (req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
     const sauceInRequest = JSON.parse(req.body.sauce)
-    const newlyUploadedImagePath = req.file.path
+    const newlyUploadedImagePath = path.join(req.file.destination, req.file.filename).replace('src\\public', '')
     const backendBaseUrl = `${req.protocol}://${req.get('host') ?? ''}`
+    console.log(newlyUploadedImagePath)
 
     await Sauces.create({
       userId: sauceInRequest.userId,
@@ -22,11 +35,22 @@ export async function createOne (req: Request, res: Response, next: NextFunction
       manufacturer: sauceInRequest.manufacturer,
       description: sauceInRequest.description,
       mainPepper: sauceInRequest.mainPepper,
-      imageUrl: backendBaseUrl + '/' + newlyUploadedImagePath,
+      imageUrl: path.join(backendBaseUrl, newlyUploadedImagePath).replace('/\\/g', '/'),
       heat: sauceInRequest.heat
     })
 
     res.json({ message: 'New sauce successfully created !' })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export async function deleteOne (req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const sauceIdInRequest = req.params.id
+    const deletionResult = await Sauces.deleteOne({ _id: sauceIdInRequest })
+    if (deletionResult.deletedCount === 0) throw new Error('There is no existing sauce to delete with this id')
+    res.json({ message: `Sauce with id ${sauceIdInRequest} has been deleted` })
   } catch (error) {
     next(error)
   }
--- src/routes/sauces.route.ts
+++ src/routes/sauces.route.ts
@@ -1,10 +1,19 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import multer from 'multer'
-import { getAll, createOne } from '../controllers/sauces.controller'
+import { getAll, getOne, createOne, deleteOne } from '../controllers/sauces.controller'
 
-const upload = multer({ dest: 'upload/' })
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: 'src/public/uploads/images/sauces',
+    filename: function (req, file, cb) {
+      cb(null, `${Date.now()}-${file.originalname}`)
+    }
+  })
+})
 
 export default Router()
   .get('/', getAll)
+  .get('/:id', getOne)
   .post('/', upload.single('image'), createOne)
+  .delete('/:id', deleteOne)
